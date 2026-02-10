ffd01ca7d359e3ee8fa987af29bd15d07df1dee6
TAUFIQ HASHMI <144479113+taufiqhashmi1@users.noreply.github.com>
2026-02-10 22:57:54 +0530

    last commit

--- global.d.ts
+++ global.d.ts
@@ -1,31 +1,20 @@
-import React from "react";
+import * as React from 'react';
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'wokwi-arduino-uno': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >;
-
-      'wokwi-led': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement> & {
-          color?: string;
-          value?: string;
-          label?: string;
-        },
-        HTMLElement
-      >;
-
-      'wokwi-pushbutton': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement> & {
-          color?: string;
-          label?: string;
-        },
-        HTMLElement
-      >;
+      'wokwi-arduino-uno': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'wokwi-led': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+        color?: string;
+        value?: string | number | boolean;
+        label?: string;
+      }, HTMLElement>;
+      'wokwi-pushbutton': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+        color?: string;
+        label?: string;
+        value?: string | number | boolean;
+      }, HTMLElement>;
+      // Add any other components like wokwi-resistor or wokwi-lcd here
     }
   }
 }
-
-export {};
